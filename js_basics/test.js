22f964e832b300e8bf58f64d5cb8cc00793ac173
Avinash Parchake <96370088+parchakeavinash@users.noreply.github.com>
2025-07-24 07:16:01 +0000

    Datatypes

--- js_basics/test.js
+++ js_basics/test.js
@@ -1,1 +1,11 @@
-console.log("avinash")
+const accountID = 123456
+let accountEmail = "avinash@gmail.com"
+var accountPassword = "56123"
+accountCity = "Pune"
+
+/*
+prefer not to use var 
+bacause of issue in block scope and functional scope
+*/
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
