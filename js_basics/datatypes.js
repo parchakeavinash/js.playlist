df1b6ba33378f5d9f88a3a97cdf3f7a5e6c426d7
Avinash Parchake <96370088+parchakeavinash@users.noreply.github.com>
2025-07-24 07:14:42 +0000

    DataTypes

---
+++ js_basics/datatypes.js
@@ -0,0 +1,21 @@
+let state;
+
+let Name = 'Avinash'
+
+/*
+Data types:
+- number
+- bigint (use for large number)
+- string => ""
+- boolean => true/ false
+- null => stadalone value  (type is object)
+- undefined 
+- symbol = unique
+
+
+*/
+
+console.log(Name);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
